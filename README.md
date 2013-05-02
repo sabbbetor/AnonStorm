dff497fee05936fcdee8d5d078dbfdc59717eb1a
sabbbetor <sabbbet@outlook.com>
2013-05-02 20:02:12 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Welcome to AnonStorm v1.1
+========================================
+
+AnonStorm is a simple TwitterStorm script written in Python.
+This tool is to make life easier for participants of activist related twitter storms.
+It's design automates your tweets using a hosted text file so you don't have to copy/paste manually.
+
+## Setup:
+
+Run the following commands :
+
+sudo apt-get install python python-dev
+sudo apt-get install python-pip
+sudo pip install -r requirements.txt
+
+### Dependencies
+
+httplib2==0.8
+oauth==1.0.1
+oauth2==1.5.211
+python-twitter==0.8.7
+tweepy==2.0
+
+=======================================
+
+You will also need to run "auth.py" and allow access of the application to your twitter account,
+"auth.py" will give you a URL, copy then paste that URL into your web browser, authorize the application.
+Your web browser will then give you a "Pin Code" from twitter, enter this Pin Code into the auth.py script,
+The script will then provide you with 2 things, an Access Token Key and an Access Token Secret, these are 
+basically encrypted versions of your user name and password. This step better protects you as the script
+will not send plaintext information.
+
+Once you have your Token information, you will need to edit "storm.py" and enter the Key and Secret
+in the spots specified in the script, they are marked as "YOUR_ACCESS_TOKEN_HERE" and "YOUR_ACCESS_SECRET_HERE".
+
+After you have everything set up there, you should have been provided with a link from either The in AnonOps
+IRC, or by an operator of your Operation, which will have a link provided by The. You will need to paste that
+link in place of "LINK_TO_OP_TWEET_FILE". 
+
+Once those are done, save the storm.py file and fire it up for your twitterstorms!
+
+If you have any issues running this application speak to sabbbet in AnonOps IRC.
+
+Original source of AnonStorm v1.0 - https://github.com/somer4ndompunk/AnonStorm 
+
+- sabbbet
---
+++ storm.py
@@ -0,0 +1,63 @@
+# AnonStorm twitterstorm bot
+#
+# Updated version v1.1 by sabbbet for @StopICMS only
+# This version uses tweepy as the older version uses old twitter API.
+#
+# This script, as it sits will pull from an online file.
+# It will pull a random tweet from the hosted file, and tweet every 2 minutes a new random tweet.
+# 
+# You MUST run the auth.py script first to obtain your access_token_key and your access_token_secret. This will authorize the app with your twitter account.
+# Once you verify the pin, you MUST update the appropriate strings access_token_key and access_token_secret with the key and secret auth.py gives you.
+#
+# A VERY special thanks to The, Happyface, Prophet and kyzersane for their work on its original script.
+#
+#
+# Version 1.1
+
+import time, datetime, sched, random
+import urllib2
+import tweepy
+     
+print('Welcome to AnonStorm v1.1 | This is developed for @StopICMS only, Happy Tweeting!')
+
+ACCESS_TOKEN_KEY='YOUR ACCESS TOKEN HERE'
+ACCESS_TOKEN_SECRET='YOUR ACCESS SECRET HERE'
+CONSUMER_KEY='5VuZ39FbuRfFdr0CpNf3zg'
+CONSUMER_SECRET='5FPi40pwS3buTMFMXm8URXoRSCubw0LqM5KtTPkdo'
+
+contents = []
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+s  = sched.scheduler(time.time, time.sleep)
+
+stat = ""
+LINK_TO_OP_TWEET_FILE = "http://pastebin.com/raw.php?i=hmW17JgF"
+
+def do_something(sc):
+        global stat
+        response = urllib2.urlopen(LINK_TO_OP_TWEET_FILE)
+        contents = response.readlines()
+        stat = contents[random.randrange(0,len(contents))]
+        status = stat[:-2]
+
+        while True:
+                try:
+                    if len(status) > 140:
+                        raise Exception(status,'\n Message is too long!')
+                    print "Tweeting...", st
+                    print "", status
+                    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+                    auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+                    api = tweepy.API(auth)
+                    result = api.update_status(status)
+                    break
+                except tweepy.error.TweepError as e:
+                        print("Twitter error received...", e.message)
+                        print("Trying again...")
+                        stat = contents[random.randrange(0,len(contents))]
+ 
+        sc.enter(120, 1, do_something, (sc,)) #Edit the "120" if you wish to speed or slow down the tweets. Time is in seconds. IE: 120 = 2 minutes
+ 
+s.enter(0, 1, do_something, (s,))
+s.run()
+
