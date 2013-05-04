a52b8b38820d52c8ea2984b970f3fcfa2db041da
sabbbetor <sabbbet@outlook.com>
2013-05-04 10:06:21 +0000

    AnonStorm now handles multiple accounts

--- storm.py
+++ storm.py
@@ -1,6 +1,6 @@
 # AnonStorm twitterstorm bot
 #
-# Updated version v1.1 by sabbbet for @StopICMS only
+# Updated version v3 by sabbbet for @StopICMS only
 # This version uses tweepy as the older version uses old twitter API.
 #
 # This script, as it sits will pull from an online file.
@@ -8,11 +8,12 @@
 # 
 # You MUST run the auth.py script first to obtain your access_token_key and your access_token_secret. This will authorize the app with your twitter account.
 # Once you verify the pin, you MUST update the appropriate strings access_token_key and access_token_secret with the key and secret auth.py gives you.
+# You can run auth.py for as many accounts you want to use the storm for.
 #
 # A VERY special thanks to The, Happyface, Prophet and kyzersane for their work on its original script.
 #
 #
-# Version 1.1
+# Version 3.0
 
 import time, datetime, sched, random
 import urllib2
@@ -20,7 +21,7 @@ import tweepy
 import sys
 import re
 
-print('\nWelcome to AnonStorm v2.0 | This is developed for @StopICMS only, Happy Tweeting!')
+print('\nWelcome to AnonStorm v3.0 | This is developed for @StopICMS only, Happy Tweeting!')
 
 ACCESS_TOKEN_KEY = ''
 ACCESS_TOKEN_SECRET = ''
@@ -31,16 +32,16 @@ print '\nGetting ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET of multiple accounts. 
 
 file_read = open("TOKENS_SECRETS.conf","rU")
 lines = file_read.read()
-
 matches = re.findall(r'KEY = ([\w\-\w]+) SECRET = ([\w\-\w]+)',lines)
-
-print matches
 looper = len(matches)
+if looper == 0:
+    print "\n\n\nYou will have to run auth.py so that it can add your ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET for one or more accounts in TOKENS_SECRETS.conf file"
+    print "\nThis is done in order to use multiple account at once for a better storm."
+    print "\nYou can run auth.py for as many accounts you want to use the storm for."
+    print "\n\n\n GoI beware, a storm is comming!"
 loopy = 0
 
 contents = []
-ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 s  = sched.scheduler(time.time, time.sleep)
 
 stat = ""
@@ -58,25 +59,26 @@ def tweet(sc):
         while loopy > 0:
             for keys_ in matches:
                 try:
+                    ts = time.time()
+                    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
                     if len(status) > 140:
                         print '\nThis [',status[:20],'...',status[-20:],'] Tweet has more than 140 characters! \n\n Please contact the owner of the paste, TheDentist on #opindia at anonops.\n'
                         stat = contents[random.randrange(0,len(contents))]
                         status = stat[:-2]
                     else:
-                        print "\nGenerating ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET for your account ... "
                         ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET = keys_
-                        print "\nTweeting ... for ACCESS_TOKEN_KEY",ACCESS_TOKEN_KEY,"and ACCESS_TOKEN_KEY",ACCESS_TOKEN_SECRET, st
+                        print "\nTweeting ... for ACCESS_TOKEN_KEY",ACCESS_TOKEN_KEY,"and ACCESS_TOKEN_SECRET",ACCESS_TOKEN_SECRET
+                        print "at", st
+                        stat = contents[random.randrange(0,len(contents))]
+                        status = stat[:-2]
                         print "", status
-                        print loopy
                         loopy = loopy - 1
-                        
                         """
                         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
                         auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
                         api = tweepy.API(auth)
                         result = api.update_status(status)
                         """
-                        break
                 except tweepy.error.TweepError as e:
                     print "\nTwitter error received...", e.message
                     print "Trying again..."
@@ -86,9 +88,7 @@ def tweet(sc):
             loopy = looper
 
        
-    sc.enter(1, 1, tweet, (sc,)) #Edit the "120" if you wish to speed or slow down the tweets. Time is in seconds. IE: 120 = 2 minutes
-
-
+    sc.enter(120, 1, tweet, (sc,)) #Edit the "120" if you wish to speed or slow down the tweets. Time is in seconds. IE: 120 = 2 minutes
 
 s.enter(0, 1, tweet, (s,))
 s.run()
