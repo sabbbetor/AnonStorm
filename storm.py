3d607b5476af3002fc47d60cc2e785fa81ad69be
sabbbetor <sabbbet@outlook.com>
2013-05-04 08:55:16 +0000

    still needs work

--- storm.py
+++ storm.py
@@ -20,24 +20,22 @@ import tweepy
 import sys
 import re
 
-print('Welcome to AnonStorm v1.1 | This is developed for @StopICMS only, Happy Tweeting!')
+print('\nWelcome to AnonStorm v3.0 | This is developed for @StopICMS only, Happy Tweeting!')
 
-ACCESS_TOKEN_KEYS = {}
-ACCESS_TOKEN_SECRETS = {}
+ACCESS_TOKEN_KEY = ''
+ACCESS_TOKEN_SECRET = ''
 CONSUMER_KEY='5VuZ39FbuRfFdr0CpNf3zg'
 CONSUMER_SECRET='5FPi40pwS3buTMFMXm8URXoRSCubw0LqM5KtTPkdo'
 
-print 'Getting ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET of multiple accounts. from TOKENS_SECRETS.txt'
+print '\nGetting ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET of multiple accounts. from TOKENS_SECRETS.txt'
 
 file_read = open("TOKENS_SECRETS.txt","rU")
 lines = file_read.read()
 
-print lines
+matches = re.findall(r'KEY = ([\w\-\w]+) SECRET = ([\w\-\w]+)',lines)
 
-matches = re.search(r'KEY = ([\w\-\w]+) SECRET = ([\w\-\w]+)',lines)
 print matches
-
-#print matches.group(1), matches.group(2)
+looper = len(matches)
 
 contents = []
 ts = time.time()
@@ -47,36 +45,43 @@ s  = sched.scheduler(time.time, time.sleep)
 stat = ""
 LINK_TO_OP_TWEET_FILE = "http://pastebin.com/raw.php?i=hmW17JgF"
 
-def do_something(sc):
-        global stat
+def tweet(sc):
+        global stat, looper, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
         response = urllib2.urlopen(LINK_TO_OP_TWEET_FILE)
         contents = response.readlines()
         stat = contents[random.randrange(0,len(contents))]
         status = stat[:-2]
+        loop = looper
 
         while True:
-                try:
-                    if len(status) > 140:
-                        print 'This [',status[:20],'...',status[-20:],'] Tweet has more than 140 characters! \n\n Please contact the owner of the paste, TheDentist on #opindia at anonops.\n'
-                        stat = contents[random.randrange(0,len(contents))]
-                        status = stat[:-2]
-                    else:
-                        print "\nTweeting...", st
-                        print "", status
-                        """
-                        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-                        auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
-                        api = tweepy.API(auth)
-                        result = api.update_status(status)
-                        """
-                        break
-                except tweepy.error.TweepError as e:
+            while loop != 0:
+                for keys_ in matches:
+                    print "Generating ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET for your account ... "
+                    ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET = keys_
+                    loop = loop - 1
+                    try:
+                        if len(status) > 140:
+                            print 'This [',status[:20],'...',status[-20:],'] Tweet has more than 140 characters! \n\n Please contact the owner of the paste, TheDentist on #opindia at anonops.\n'
+                            stat = contents[random.randrange(0,len(contents))]
+                            status = stat[:-2]
+                        else:
+                            print "\nTweeting...", st
+                            print "", status
+                            """
+                            auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+                            auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+                            api = tweepy.API(auth)
+                            result = api.update_status(status)
+                            """
+                            break
+                    except tweepy.error.TweepError as e:
                         print("\nTwitter error received...", e.message)
                         print("Trying again...")
                         stat = contents[random.randrange(0,len(contents))]
- 
-        sc.enter(1, 1, do_something, (sc,)) #Edit the "120" if you wish to speed or slow down the tweets. Time is in seconds. IE: 120 = 2 minutes
- 
-s.enter(0, 1, do_something, (s,))
+            sc.enter(60, 1, tweet, (sc,)) #Edit the "120" if you wish to speed or slow down the tweets. Time is in seconds. IE: 120 = 2 minutes
+
+
+
+s.enter(0, 1, tweet, (s,))
 s.run()
 
