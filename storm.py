2e7439fcca06f78447beac1f0b968b45ab7ffd55
sabbbetor <sabbbet@outlook.com>
2013-05-04 10:56:04 +0000

    minor changes

--- storm.py
+++ storm.py
@@ -21,26 +21,28 @@
 import time, datetime, sched, random
 import urllib2
 import tweepy
+import sys
 import re
 
-print('\nWelcome to AnonStorm v2.0 | This is developed for @StopICMS only, Happy Tweeting!')
+print('\n Welcome to AnonStorm v2.0 | This is developed for @StopICMS only, Happy Tweeting!')
 
 ACCESS_TOKEN_KEY = ''
 ACCESS_TOKEN_SECRET = ''
 CONSUMER_KEY='5VuZ39FbuRfFdr0CpNf3zg'
 CONSUMER_SECRET='5FPi40pwS3buTMFMXm8URXoRSCubw0LqM5KtTPkdo'
 
-print '\nGetting ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET of multiple accounts. from TOKENS_SECRETS.txt'
+print '\n Getting ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET of multiple accounts. from TOKENS_SECRETS.txt'
 
 file_read = open("TOKENS_SECRETS.conf","rU")
 lines = file_read.read()
 matches = re.findall(r'KEY = ([\w\-\w]+) SECRET = ([\w\-\w]+)',lines)
 looper = len(matches)
 if looper == 0:
-    print "\n\n\nYou will have to run auth.py so that it can store your ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET keys of one or more accounts in TOKENS_SECRETS.conf file."
-    print "\nThis is done in order to use multiple account at once for a better storm."
-    print "\nYou can run auth.py for as many accounts you want to use the storm for."
+    print "\n\n\n You will have to run auth.py so that it can store your ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET of one or more accounts in TOKENS_SECRETS.conf file."
+    print "\n This is done in order to use multiple account at once for a better storm."
+    print "\n You can run auth.py for as many accounts you want to use the storm for."
     print "\n\n\n GoI beware, a storm is comming!"
+    sys.exit(1)
 loopy = 0
 
 contents = []
@@ -75,12 +77,10 @@ def tweet(sc):
                         status = stat[:-2]
                         print "", status
                         loopy = loopy - 1
-                        """
                         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
                         auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
                         api = tweepy.API(auth)
                         result = api.update_status(status)
-                        """
                 except tweepy.error.TweepError as e:
                     print "\nTwitter error received...", e.message
                     print "Trying again..."
